21f653a637a37b38e94fe8930c808bfbf7437b62
Tim Vanloocke <tim.vanloocke@roeselare.be>
2020-04-28 17:13:17 +0200

    added config

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
-module.exports = {
-  mongoURI:
-    "mongodb+srv://samentuin-rw:jbzuFTi5RgJTaTk2@samentuin-db-rzcer.mongodb.net/test?retryWrites=true&w=majority",
-  secretOrKey: "Zp3ulMTujU7&",
-};
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./keys_prod");
+} else {
+  module.exports = require("./keys_dev");
+}
---
+++ config/keys_dev.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://samentuin-rw:jbzuFTi5RgJTaTk2@samentuin-db-rzcer.mongodb.net/test?retryWrites=true&w=majority",
+  secretOrKey: "Zp3ulMTujU7&",
+};
